b3a792cf5d2e72e87a999140ffc8a659ceca1a19
mercy-ndiritu <mewanjikundiritu@gmail.com>
2024-04-13 16:38:30 +0300

    second commit

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id =sec.getAttribute('id');
 
-        if(top >= && top <offset + height){
+        if(top >= offset && top <offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
 
         };
     });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky' , window.scrollY > 100);
 };
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading' , { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
